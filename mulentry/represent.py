95c6e19ce668ef993aa062dfafa53425066d496c
MCruces-fz <mcsquared.fz@gmail.com>
2021-06-28 14:00:20 +0200

    Start with Represent class

--- mulentry/represent.py
+++ mulentry/represent.py
@@ -1,6 +1,12 @@
+from mulentry.calc import Calc
 
 
 class Represent:
-    def __init__(self):
-        pass
+    def __init__(self, calc: Calc):
+        self.calc = calc
 
+    def map_marginal(self):
+        """
+        Get inspired from map_marginal from original tr_ana_entries...py
+        """
+        pass
